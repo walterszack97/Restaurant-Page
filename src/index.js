a976a36bb10eade50b3f45ed66ffb3d3df8bbf8f
zachary w <walterszack97@gmail.com>
2023-05-16 23:08:18 -0400

    Initialize and test

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles.css";
+import { pageLoad } from "./pageload";
+console.log("working");
+pageLoad();
---
+++ src/pageload.js
@@ -0,0 +1,10 @@
+const pageLoad = () => {
+  console.log("pageload working");
+  /* const container = document.querySelector("#content");
+    const mainPageContainer = document.createElement("div");
+    mainPageContainer.setAttribute("id", "mainpage-container");
+  
+    container.appendChild(mainPageContainer); */
+};
+
+export { pageLoad };
